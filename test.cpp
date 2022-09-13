e7bd2622b489f73795fe33a87ea200327286edcf
Naruto9811 <peilin_liu@foxmail.com>
2022-09-13 13:44:48 +0800

    test

--- test.cpp
+++ test.cpp
@@ -2,9 +2,18 @@
 #include <eigen3/Eigen/Core>
 #include <opencv4/opencv2/alphamat.hpp>
 
+using namespace Eigen;
+using namespace std;
+
 int main (int argc, char** argv)
 {
-    Eigen::Matrix<float,2,3> matrix;
+    Matrix<float,2,3> matrix;
+    Matrix2d matrix1;
+    int input_dim;
+    cin >> input_dim;
 
+
+
+    return 0;
 }
 
